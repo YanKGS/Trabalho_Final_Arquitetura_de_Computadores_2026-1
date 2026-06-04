e97bbf1db6ae256ffc8f9831eb34afa029bbe50b
Yan <Yan Kaue>
2026-06-04 18:43:02 -0300

    [add] teste da cache

---
+++ cache.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include <time.h>
+
+#define KB 1024
+#define MB 1024 * 1024
+
+int main(){
+   
+   long start;
+   int lengthMod = 8*MB -1;
+   double totalTime, timeTaken;
+   int tmp;
+
+   static int array[8*MB];
+   int i,j;
+   unsigned int k;
+   for(i=1;i<=2048;i*=2){
+      totalTime=0;
+      for(j=0;j<6;j++){
+         start = clock();
+         for(k=0;k<1024*MB;k += 5){
+            tmp +=array[(k*i)&lengthMod];   
+         }
+         timeTaken = (double)(clock() - start)/CLOCKS_PER_SEC;
+         totalTime += timeTaken;
+      }
+      totalTime /= 6;
+      printf("For i value: %d    Time Taken:%lf\n",i,totalTime);
+   } 
+}
